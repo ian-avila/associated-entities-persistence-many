70ab42508b0edeede4a870364d6d0bae65c16f77
ian-avila <ian.avg07@gmail.com>
2026-04-23 16:25:26 -0300

    Chore: Configure initial setup, Create Profile 'test', Set up h2-database

---
+++ src/main/java/com/ian_avila/associated_entities_persistence_many/AssociatedEntitiesPersistenceManyApplication.java
@@ -0,0 +1,13 @@
+package com.ian_avila.associated_entities_persistence_many;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociatedEntitiesPersistenceManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssociatedEntitiesPersistenceManyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ian_avila/associated_entities_persistence_many/AssociatedEntitiesPersistenceManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ian_avila.associated_entities_persistence_many;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociatedEntitiesPersistenceManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
